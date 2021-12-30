4838c1d82ebfbffe2e76d1086b4d99185083942a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-12-30 19:57:28 +0800

    Create author-sol.cpp

---
+++ author-sol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int x1, x2;
+        cin >> x1 >> x2;
+		// the answer is YES if x1 is greater than or equal to x2
+        string ans = (x1 >= x2 ? "YES" : "NO");
+		cout << ans << '\n';
+    }
+    return 0;
+}
